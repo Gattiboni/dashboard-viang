3522d476a4e1c46e0ca6d0f755d5fedcace52fad
Alan Gattiboni <alangattiboni@gmail.com>
2025-12-16 08:56:42 -0300

    pasta micro-fronts criada para embed com railway

---
+++ micro-fronts/metabase-embed-home/server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Erro ao carregar o micro-front");
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
+        res.end(content);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Micro-front embed rodando na porta ${PORT}`);
+});
