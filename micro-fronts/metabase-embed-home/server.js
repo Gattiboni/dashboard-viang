bfc09e261d46ce8832eba180d8182850c887097b
Alan Gattiboni <alangattiboni@gmail.com>
2025-12-16 09:13:49 -0300

    fix: force server to listen on 8080 for Railway

--- micro-fronts/metabase-embed-home/server.js
+++ micro-fronts/metabase-embed-home/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(__dirname, "index.html");
